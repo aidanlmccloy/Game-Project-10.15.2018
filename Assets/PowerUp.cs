9ce17bffd8e1cba54bdd7be0101304ee9074fcc8
aidanlmccloy <43994783+aidanlmccloy@users.noreply.github.com>
2018-10-12 09:22:54 -0400

    Test Commit for Game Project

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Untagged"))
+        {
+            print(other.tag);
+            //print(other.CompareTag("Untagged"));
+            Destroy(gameObject);
+        }
+    }
+
+}
