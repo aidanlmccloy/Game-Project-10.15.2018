cffb0297cdcdf519c5ae4bb8161015691afe0fcd
aidanlmccloy <43994783+aidanlmccloy@users.noreply.github.com>
2018-10-12 20:36:19 -0400

    Update 1.0

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    public float multiplier = 10f;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Untagged"))
         {
             print(other.tag);
             //print(other.CompareTag("Untagged"));
+            Rigidbody stats = other.GetComponent<Rigidbody>();
+            //stats.mass *= multiplier; 
             Destroy(gameObject);
         }
     }
--- Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
@@ -87,21 +87,21 @@ namespace UnityStandardAssets.Vehicles.Car
         private void Update()
         {
             // get the distance to main camera
-            float camDist = (Camera.main.transform.position - transform.position).sqrMagnitude;
+            //float camDist = (Camera.main.transform.position - transform.position).sqrMagnitude;
 
             // stop sound if the object is beyond the maximum roll off distance
-            if (m_StartedSound && camDist > maxRolloffDistance*maxRolloffDistance)
+            //if (m_StartedSound && camDist > maxRolloffDistance*maxRolloffDistance)
             {
-                StopSound();
+               StopSound();
             }
 
             // start the sound if not playing and it is nearer than the maximum distance
-            if (!m_StartedSound && camDist < maxRolloffDistance*maxRolloffDistance)
+            //if (!m_StartedSound && camDist < maxRolloffDistance*maxRolloffDistance)
             {
-                StartSound();
+               StartSound();
             }
 
-            if (m_StartedSound)
+            //if (m_StartedSound)
             {
                 // The pitch is interpolated between the min and max values, according to the car's revs.
                 float pitch = ULerp(lowPitchMin, lowPitchMax, m_CarController.Revs);
